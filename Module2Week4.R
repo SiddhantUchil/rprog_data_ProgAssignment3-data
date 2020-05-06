7015205eda6f67ba672a04939992e615a4c5c575
Siddhant Uchil <siddhant.uchil@somaiya.edu>
2020-05-06 22:15:56 +0530

    Task 3

--- Module2Week4.R
+++ Module2Week4.R
@@ -32,13 +32,56 @@ best = function(state1, outcome){
   
   names(file3) = c("hospital", "state", "outcome")
   
-  sortedfile = arrange(file3, outcome, hospital)
+  sortedfile <<- arrange(file3, outcome, hospital)
   
   sortedfile
+  
 }
 
-best("AL", "heart attack")
 
+best("TX", "heart attack")
+
+
+rankhospital = function(state1, outcome, num){
+  
+  main2
+  file1 = main2[,c(2,7,11,17,23)]
+  names(file1) = c("hospital","state","attack","failure", "pneumonia")
+  
+  file2 = split(file1, file1$state)
+  file3 = data.frame(file2[state1])
+  
+  if(outcome == "heart attack"){
+    file3 = file3[, c(1,2,3)]
+  } else if (outcome == "heart failure"){
+    file3 = file3[, c(1,2,4)]
+  } else {
+    file3 = file3[, c(1,2,5)]
+  }
+  
+  file3 = na.omit(file3)
+  
+  names(file3) = c("hospital", "state", "outcome")
+  
+  sortedfile <<- arrange(file3, outcome, hospital)
+  
+  b <<- sortedfile
+  
+  {
+    if(num == "best"){
+      b = b[1, "hospital"]
+    } else if(num == "worst"){
+        c = nrow(b)
+        b = b[c, "hospital"]
+      }
+      else{
+      b = b[num, "hospital"]
+    }
+  }
+  b
+}
+
+rankhospital("TX", "heart failure", "worst")
 
 
 
